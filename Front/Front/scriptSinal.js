4503095c2a725aac6fe3021ee298750f99a35a86
Matheus <matias.vicio8@gmail.com>
2020-11-24 20:00:18 -0400

    Comunicação Front-Back

--- Front/Front/scriptSinal.js
+++ Front/Front/scriptSinal.js
@@ -1,20 +1,30 @@
 function subir() {
-    var titulo = document.getElementById('titulo').value;
+    var titulo1 = document.getElementById('titulo').value;
     var descri = document.getElementById('descri').value;
     var tituloLink = document.getElementById('linktitulo').value;
     var descriLink = document.getElementById('linkdescri').value;
 
-    console.log(titulo,descri,tituloLink,descriLink);
+    console.log(titulo1,descri,tituloLink,descriLink);
     axios.post('http://localhost:8080/api/verbete',
-        {
-            "titulo": titulo,
-            "descricao": descri,
-            "tituloLibras": tituloLink,
-            "descricaoLibras": descriLink
+        {withCredentials: true,
+            auth: {
+                titulo: titulo1.toString(),
+                descricao: descri.toString(),
+                tituloLibras: tituloLink.toString(),
+                descricaoLibras: descriLink.toString()
+            },
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Authorization",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+                "Content-Type": "application/json;charset=UTF-8"
+            },
         })
         .then(function (response) {
+            console.log("teste")
             console.log(response.data);
         }).catch(function (response) {
+            console.log("blah")
         console.log(response.data)
     });
 }
